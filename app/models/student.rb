6a918d3cf9c1bf93c2b69a3e8df06e71ae97d675
Kristine Hines <hines.kristine@gmail.com>
2013-04-18 19:57:51 -0700

    created student class with attributes

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,24 @@
 require_relative '../../db/config'
 
 # implement your Student model here
+class Student <ActiveRecord::Base
+  attr_accessible :name, :age
+
+  def assign_attributes(args)
+    @first_name = args[:first_name]
+    @last_name = args[:last_name]
+    @gender = args[:gender]
+    @birthday = args[:birthday]
+  end
+  
+
+  def name
+    @name = "#{@first_name} #{@last_name}"
+  end
+
+  def age
+    now = Date.today
+    @age = now.year - @student.birthday.year - ((now.month > @student.birthday.month || (now.month == @student.birthday.month && now.day >= @student.birthday.day)) ? 0 : 1)
+  end
+ 
+end
